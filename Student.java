4004d24ce5da3e6d6c58e675198305b5ca42159a
Ganesh <ggpatekarinfo@gmail.com>
2018-10-08 01:13:22 +0530

    First commit: added Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+package SchoolManagement;
+class Student{
+    private String name;
+	
+	Student(String name){
+		if(name ==null){
+		System.out.println("Invalid name");
+		}
+		this.name = name;
+	}
+	
+	
+}
